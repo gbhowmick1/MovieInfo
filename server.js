40e081f632cc38004fccceeb96241ed2f788ed31
gbhowmick1 <gbhowmick1989@gmail.com>
2021-03-15 14:54:33 +0530

    after adding Procfile

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/movie-info-search"));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/movie-info-search/index.html"));
-});
+
+app.use(express.static(path.join(__dirname, './dist/movie-info-search')));
+
+app.use('/', express.static(path.join(__dirname,'/dist/movie-info-search/index.html')));
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname , "/dist/movie-info-search/index.html"));
+// });
 app.listen(process.env.PORT || 8080);
